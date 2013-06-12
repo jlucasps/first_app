0cd85b87a2891137e7189316d4ba5c45602ed151
João Lucas <jlucasps@gmail.com>
2013-06-12 11:28:58 -0300

    Navegação em páginas públicas

--- spec/features/welcome/index.html.erb_spec.rb
+++ spec/features/welcome/index.html.erb_spec.rb
@@ -7,4 +7,18 @@ describe ".index", :type => :feature do
     page.should have_content("Hello, world!")
   end
 
+  it "access public pages" do
+    visit index_path
+    page.should have_content("Hello, world!")
+
+    click_link "About"
+    page.should have_content("About us")
+
+    click_link "Contact"
+    page.should have_content("How to find us")
+
+    click_link "Home"
+    page.should have_content("Hello, world!")
+  end
+
 end
